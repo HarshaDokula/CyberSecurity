ecf0d2a724016f72247615e4f44a06e493f89acf
HarshaDokula <harshadokula@gmail.com>
2018-06-18 15:33:48 -0700

    updatemsg

--- upload.bat
+++ upload.bat
@@ -8,7 +8,7 @@ rem enter the message
 SET /P _updatemsg= Please enter an message:
 
 rem commit with the given message 
-git commit -m "%updatemsg"
+git commit -m %updatemsg
 
 rem push master branch of this remote project
 git push
