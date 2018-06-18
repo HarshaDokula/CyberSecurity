ec5fbfa094a6cf13608ced0e800b3ad938fb5dd4
HarshaDokula <harshadokula@gmail.com>
2018-06-18 15:31:48 -0700

    updatemsg

--- upload.bat
+++ upload.bat
@@ -4,8 +4,11 @@ git status
 rem add all files
 git add .
 
+rem enter the message
+SET /P _updatemsg= Please enter an message:
+
 rem commit with the given message 
-git commit -m "on 18thjune2018"
+git commit -m "%updatemsg"
 
 rem push master branch of this remote project
 git push
