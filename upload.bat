05dd254ac37778702aac90ced66970f8d3db72e1
HarshaDokula <harshadokula@gmail.com>
2018-06-18 15:21:08 -0700

    on 18thjune2018

---
+++ upload.bat
@@ -0,0 +1,13 @@
+rem check status
+git status
+
+rem add all files
+git add .
+
+rem commit with the given message 
+git commit -m "on 18thjune2018"
+
+rem push master branch of this remote project
+git push
+
+pause
